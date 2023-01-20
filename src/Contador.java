10fa85446687db2657109a5997884b693e043f33
severino gouveia <severinogouveia_@hotmail.com>
2023-01-20 16:58:02 -0300

    Projeto finalizado

--- src/Contador.java
+++ src/Contador.java
@@ -4,34 +4,38 @@
 //Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar a exceção customizada chamada de
 // ParametrosInvalidosException com a segunda mensagem: "O segundo parâmetro deve ser maior que o primeiro"
 
-import java.text.ParseException;
 import java.util.Scanner;
 
 public class Contador {
-	public static void main(String[] args) {
-	
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
-		
-		try {
-            //chamando o método contendo a lógica de contagem
-				contar(parametroUm, parametroDois);
-        }catch (Exception e ) {
-			System.out.println(e.getMessage());
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+    public static void main(String[] args) throws ParametrosInvalidosException {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception);
         }
+
     }
-		
+
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         if (parametroUm > parametroDois)
-		throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
 
         int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
         for (int i = 1;i <=contagem ;i++){
             System.out.println("Imprimindo o número " + i);
+        }
+    }
+
+    private static class ParametrosInvalidosException extends Exception {
+        public ParametrosInvalidosException(String errorMessage) {
+            super(errorMessage);
         }
     }
 }
